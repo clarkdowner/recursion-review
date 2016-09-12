f33ba1966cfe45164bc4d627a47e08668fa760a8
clarkdowner <cddowner@gmail.com>
2016-09-12 11:37:04 -0700

    Pass tests for stings and arrays

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,6 +5,7 @@
 
 var stringifyJSON = function(obj) {
   // your code goes here
+  var results = [];
 
   if (typeof obj === "boolean" || typeof obj === "number"){
     return '' + obj;
@@ -14,4 +15,17 @@ var stringifyJSON = function(obj) {
     return "null";
   }
 
+  if (typeof obj === "string") {
+    return '"' + obj + '"';
+  }
+
+  if (Array.isArray(obj)) {
+    for(var i = 0; i < obj.length; i++) {
+      results.push(stringifyJSON(obj[i]));
+      //if (i !== obj.length - 1) {
+      //  results.push(',');
+      //}
+    }
+    return '[' + results.join(',') + ']'
+  }
 };
